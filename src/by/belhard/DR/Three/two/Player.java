805b824ad8e85046bffc0e460b80523298b58497
igorek19882020 <github.com/igorek19882020/Igor>
2020-02-07 17:08:33 +0300

    !

---
+++ src/by/belhard/DR/Three/two/Player.java
@@ -0,0 +1,19 @@
+package by.belhard.DR.Three.two;
+
+import java.util.Scanner;
+
+public class Player {
+
+    public void s() {
+
+        double x = 0;
+        double y = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите координату x ");
+        x = sc.nextDouble();
+        System.out.println("Введите координату y ");
+        y = sc.nextDouble();
+        System.out.println("Текущая координата х - " + x + " Текущая координата y - " + y);
+    }
+
+}
