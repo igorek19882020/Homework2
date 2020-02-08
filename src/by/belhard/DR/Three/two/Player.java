68c191ddd63c4c237cba9a06357727b7321aeaf9
igorek19882020 <github.com/igorek19882020/Igor>
2020-02-08 16:21:50 +0300

    !

--- src/by/belhard/DR/Three/two/Player.java
+++ src/by/belhard/DR/Three/two/Player.java
@@ -3,9 +3,7 @@ package by.belhard.DR.Three.two;
 import java.util.Scanner;
 
 public class Player {
-
-    public void s() {
-
+    public void r() {
         double x = 0;
         double y = 0;
         Scanner sc = new Scanner(System.in);
@@ -13,7 +11,24 @@ public class Player {
         x = sc.nextDouble();
         System.out.println("Введите координату y ");
         y = sc.nextDouble();
-        System.out.println("Текущая координата х - " + x + " Текущая координата y - " + y);
+        System.out.println("Текущee положение Player:(" + (x + "," + y) + ")");
     }
 
+    public void Q() {
+        double x = 0;
+        double y = 0;
+        double x1 = 0;
+        double y1 = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите координату x ");
+        x = sc.nextDouble();
+        System.out.println("Введите координату y ");
+        y = sc.nextDouble();
+        System.out.println("Введите координату x1 ");
+        x1 = sc.nextDouble();
+        System.out.println("Введите координату y1 ");
+        y1 = sc.nextDouble();
+        double S = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+        System.out.println(S);
+    }
 }
