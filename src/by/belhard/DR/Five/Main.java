85124b615066d1995667bf7b7aeae01f2d4b50cc
igorek19882020 <github.com/igorek19882020/Igor>
2020-02-12 15:29:08 +0300

    1

--- src/by/belhard/DR/Five/Main.java
+++ src/by/belhard/DR/Five/Main.java
@@ -2,7 +2,7 @@ package by.belhard.DR.Five;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ejhroiufhrf");
+        System.out.println("ejhroiufhrfrff");
     }
 
 
